25cba85b7f8ab5af6713db9618e37ee53bfeda0f
Ahmed Salem <ahmedsalem.me.as@gmail.com>
2021-06-07 00:19:00 +0200

    i

--- parenting (SpecialistApp)/lib/models/transaction.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Transaction {
-  final String id;
-  final DateTime time;
-  final DateTime date;
-
-  Transaction({@required this.id, @required this.time, @required this.date});
-}
